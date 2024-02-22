6fcdb72613c1203d68702833e257cb221a249275
Cherie Yuen <cherieyuenn@gmail.com>
2024-02-21 19:16:16 -0500

    initialize the proj

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final Project
+
+## By: Cherie Yuen
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Title</h2>
+    </div>
+  )
+}
+
+export default App
